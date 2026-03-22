6c0d9c9340975ba5bcc2ef201cb3b59fb036662c
katuUushkinzzz <223103204+katuUushkinzzz@users.noreply.github.com>
2026-03-22 17:27:05 +0300

    добавила карту

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MapContainer
+      center={[47.210, 38.925]}
+      zoom={14}
+      zoomControl={false}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors"
+      />
+      <ZoomControl position="topright" />
+    </MapContainer>
   );
 }
-
-export default App;
