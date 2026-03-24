7d19dcb02416558fbb652cbff5687b5e527b2ed5
katuUushkinzzz <223103204+katuUushkinzzz@users.noreply.github.com>
2026-03-24 22:07:39 +0300

    add project files

---
+++ app/components/map.tsx
@@ -0,0 +1,21 @@
+'use client'; 
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import './map.css';
+
+export default function Map() {
+  return (
+    <MapContainer
+      center={[47.210, 38.925]}
+      zoom={14}
+      zoomControl={false}
+      style={{ height: '100vh', width: '100%' }}
+    >
+      <TileLayer
+        url="https://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors"
+      />
+      <ZoomControl position="topright" />
+    </MapContainer>
+  );
+}
