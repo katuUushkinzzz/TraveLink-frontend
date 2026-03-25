d43e41dd525affcb1c1f7dd5b828653e0febcc02
RENOMIZER <94466218+RENOMIZER@users.noreply.github.com>
2026-03-26 01:23:49 +0300

    Temp stuff will revert later

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,102 @@
-import Image from "next/image";
+'use client'
+
+import SearchBar from "./components/SearchBar/SearchBar";
+import { RouteData, RoutePanel } from "./components/SidePanel/RoutePanel";
+import SidePanel from "./components/SidePanel/SidePanel";
+import { useRef, useState } from "react";
 
 export default function Home() {
+  const [isPanelShown, setPanelShown] = useState(true);
+  const [routeData, setRouteData] = useState<RouteData>();
+  const sidePanelRef = useRef<HTMLDivElement>(null);
+  const [routes, setRoutes] = useState<RouteData[]>([
+      {
+          id: 0,
+          author: 'Jonh Doe',
+          authorPfp: '/search-window/checker.png',
+          creationDate: '01 января 1970',
+          routeName: 'Placeholder Route' ,
+          routeDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          routeTags: ['test1', 'test2', 'test3', 'test4'],
+          likeCount: 1337,
+          commentCount: 420,
+          points: [{
+            pointName: 'Парк Альпак Пача Мама',
+            pointType: 'Парк',
+            pointLocation: 'пр-кт Сельмаш, 1А Парк культуры и отдыха им. Николая Островского, Ростов-на-Дону',
+            pointRating: 4.9,
+            ratingCount: 912,
+            pointDescription: 'Пача Мама - уютный уголок природы в Ростове-на-Дону, где обитают милые альпаки из Перу, ласковые кролики и ручные козы. Ко всем животным можно зайти в вольеры, покормить, погладить и даже обнять! Зарядитесь альпакотерапией и порцией позитива!',
+            nextDistance: 1160,
+            nextTime: 13
+          },
+          {
+            pointName: 'Парк Альпак Пача Мама',
+            pointType: 'Парк',
+            pointLocation: 'пр-кт Сельмаш, 1А Парк культуры и отдыха им. Николая Островского, Ростов-на-Дону',
+            pointRating: 4.9,
+            ratingCount: 912,
+            pointDescription: 'Пача Мама - уютный уголок природы в Ростове-на-Дону, где обитают милые альпаки из Перу, ласковые кролики и ручные козы. Ко всем животным можно зайти в вольеры, покормить, погладить и даже обнять! Зарядитесь альпакотерапией и порцией позитива!',
+            nextDistance: 1160,
+            nextTime: 13
+          },
+          {
+            pointName: 'Парк Альпак Пача Мама',
+            pointType: 'Парк',
+            pointLocation: 'пр-кт Сельмаш, 1А Парк культуры и отдыха им. Николая Островского, Ростов-на-Дону',
+            pointRating: 4.9,
+            ratingCount: 912,
+            pointDescription: 'Пача Мама - уютный уголок природы в Ростове-на-Дону, где обитают милые альпаки из Перу, ласковые кролики и ручные козы. Ко всем животным можно зайти в вольеры, покормить, погладить и даже обнять! Зарядитесь альпакотерапией и порцией позитива!',
+          }],
+          isLiked: false,
+          image: '/search-window/checker.png'
+      },
+      {
+          id: 1,
+          author: 'Jane Doe',
+          authorPfp: '/search-window/checker.png',
+          creationDate: '02 января 1970',
+          routeName: 'New Placeholder Route' ,
+          routeDescription: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          routeTags: ['test4', 'test3', 'test2', 'test1'],
+          likeCount: 228,
+          commentCount: 413,
+          points: [],
+          isLiked: false,
+          image: '/search-window/checker.png'
+      }
+  ]);
+
+  const toggleLike = (id: number) => {
+      setRoutes(routes.map(route => {
+          if (route.id === id) {
+              const isNowLiked = !route.isLiked;
+              const updatedRoute = {
+                  ...route,
+                  isLiked: isNowLiked,
+                  likeCount: isNowLiked ? route.likeCount + 1 : route.likeCount - 1
+              };
+
+              if (routeData?.id === id) {
+                  setRouteData(updatedRoute)
+              }
+
+              return updatedRoute;
+          }
+          return route;
+      }));
+    };
+
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      <SidePanel
+        isPanelShown={isPanelShown}
+        setRouteData={setRouteData}
+        sidePanelRef={sidePanelRef}
+        routes={routes}
+        onLiked={toggleLike} />
+      <SearchBar setPanelShown={setPanelShown} isPanelShown={isPanelShown}/>
+      <RoutePanel sidePanelRef={sidePanelRef} routeData={routeData} onLiked={() => toggleLike(routeData?.id as number)}/>
     </div>
   );
 }
